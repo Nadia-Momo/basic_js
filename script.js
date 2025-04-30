15c0c1bed8b4ac083140dbadd51e178683394512
Labib361 <ahsanulmahbub1999@gmail.com>
2025-04-30 09:10:42 +0600

    basic js concept added

---
+++ script.js
@@ -0,0 +1,61 @@
+console.log(33);
+var weight=38;
+var price=38;
+var year=1995;
+var money=2005;
+var age=12;
+console.log(age);
+var name="sadaruddin";
+var address="Ändar killa bander ban";
+var country="Bangladesh";
+var isRich=true;
+var passed=false;
+console.log(passed);
+console.log(typeof passed)
+console.log(country);
+var price=555;
+console.log(typeof price);
+//how to write variable name
+var my_current_home_address="Uttara,Dhaka";//snake case
+var myCurrentHomeAddress="Uttara,Dhaka";//camel case
+var MyCurrentHomeAddress="Uttara,Dhaka";//Pascal case
+var orangePrice=20;
+var chocolate=0.5;
+var applePrice=parseInt('26');
+var applePrice=parseFloat('26.908');
+console.log(typeof orangePrice);
+console.log(typeof applePrice);
+console.log(orangePrice + applePrice);
+var first=0.1;
+var second=0.2;
+var total=first+second;
+console.log(total.toFixed(3));
+const num=-50/0;
+console.log(num);
+var price=35;
+price+=35;
+console.log(price);
+var fridge=35000;
+fridge=fridge-1000;
+console.log(fridge);
+var total_money=1000;
+var total_price=700;
+var remainder=total_money-total_price;
+console.log("Remainder is ",remainder);
+var math=75.25;
+var bio=65;
+var chemistry=80;
+var physics=35.45;
+var bangla=99.50;
+var total=math+bio+chemistry+physics+bangla;
+console.log(total);
+var avg=(total/5).toFixed(3);
+
+console.log("Average is ",avg);
+var givenNumber=119;
+var remainder=119%5;
+console.log("Remainder is ",remainder);
+const a=isNaN('11');
+console.log(a);
+const b=isNaN(2-10);
+console.log(b);
