21863e1eb0a6ccd32c9c490e0759e1d9435f1b88
Labib361 <ahsanulmahbub1999@gmail.com>
2025-04-30 15:16:13 +0600

    nested if else part done

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 console.log(33);
 var weight=38;
-var price=38;
+// var price=38;
 var year=1995;
 var money=2005;
 var age=12;
@@ -59,3 +59,86 @@ const a=isNaN('11');
 console.log(a);
 const b=isNaN(2-10);
 console.log(b);
+var sunglass=80;
+//number
+var subject="EEE"
+//boolean
+var passed=true;
+var lastExamScore=89;
+// operation + - * \ %
+// +=,-=,*=,/=
+// comparison
+// 1.bigger :>
+// 2.less:<
+//3.equal:===
+//4.grater than or equal >=
+//5.less than or equal <=
+var weight=20;
+if(weight<20){
+    console.log("I will carry it myself");
+}
+else{
+    console.log("i will rent a rickshaw")
+}
+const salary=25000;
+const isBCS=true;
+const height=61;
+
+if(salary>20000 && height >66){
+    console.log('supatro');
+}
+else{
+    console.log("onno patro khuji");
+}
+if(salary>25000 || height>72 || isBCS==true){
+console.log("eso baba kobul");
+}
+else{
+    console.log("vaag tui moqbul");
+}
+const prise=500;
+if(prise>=5000){
+    const discount=prise*10/100;
+    const payAmount=prise-discount;
+    console.log(payAmount);
+}
+else if(price<5000){
+    const discount=prise*5/100;
+    const payAmount=prise-discount;
+    console.log(payAmount);  
+}
+else{
+    console.log(5000);
+}
+// const age=17;
+// if(age<12){
+//     console.log("you can eat for free");
+// }
+// else if(age>=60){
+//     const discount=price*50/100;
+//     const payAmount=price-discount;
+//     console.log(payAmount);
+// }
+// else if(age>=40){
+
+// }
+// else{
+//     console.log(price);
+// }
+const money1=0;
+if(money1>300){
+    console.log('bro you are rich');
+}
+else{
+    if(money1>100){
+        console.log('tui gorib o na borolok o na');
+    }
+    else{
+        if(money1>0){
+            console.log('dosto kola kha.calcium bara');
+        }
+        else{
+            console.log('tui amer bondhu na');
+        }
+    }
+}
